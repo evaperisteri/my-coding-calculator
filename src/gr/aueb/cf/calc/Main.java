7031f3eee556ea84f1d35d67c21d33ccaa0acbd7
evaperisteri <evangelia.peristeri.g@gmail.com>
2024-12-17 18:54:07 +0200

    hotfix completed. try catch and exception

--- src/gr/aueb/cf/calc/Main.java
+++ src/gr/aueb/cf/calc/Main.java
@@ -13,6 +13,11 @@ public class Main {
     }
 
     public static int div(int a, int b)  {
-        return a/b;
+       try {
+           return a / b;
+       } catch (ArithmeticException e) {
+           System.err.println("error.denominator must not be zero");
+           throw e;
+       }
     }
 }
